772de04aa5bb4d87c084c2c986a0c5accd5fff87
Nika Ablao <nfa@cornell.edu>
2018-02-27 16:07:35 -0500

    Changed everything to html

--- php/header.php
+++
@@ -1,8 +0,0 @@
-<a href="../index.php"><img id="icon" src="../img/icon.png"></a>
-	<div id="menu">
-	    <ul>
-	        <li><a href="../index.php">WORK</a></li>
-	        <li><a href="about.php">ABOUT</a></li>
-	        <li><a target="_blank" href="../NAblao_Resume.pdf">RESUME</a></li>
-	    </ul>
-	</div>
